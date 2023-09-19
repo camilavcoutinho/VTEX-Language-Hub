17a33e96544f2d78a2dfd1b31036f8c5044d8db4
sheilagomes <sheilagomes1@gmail.com>
2023-09-19 15:17:31 -0300

    Add iframe content change

--- content/internal.js
+++ content/internal.js
@@ -2,3 +2,11 @@ let hamburger = document.querySelector(".hamburger");
 	hamburger.addEventListener("click", function(){
 		document.querySelector("body").classList.toggle("active");
 	})
+
+function styleGuide() {
+    document.getElementById("main-frame").src = "./loc_style_guides/Localization Style Guide - PT/VTEXPortugueseStyleGuidePT.html";
+}
+
+function adminReviewLog() {
+    document.getElementById("main-frame").src = "./loc_admin_review_logs/Admin String Standardization - EN/VTEXAdminstringstandardizationinEnglish.html";
+}
