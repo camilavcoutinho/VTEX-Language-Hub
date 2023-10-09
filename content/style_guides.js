9f8f57989e07ded0a0080476bdf9e2d29e69243d
georgelimadev <george.lima@vtex.com.br>
2023-10-09 08:27:56 -0300

    Adding open/close menu behavior on Style Guides

---
+++ content/style_guides.js
@@ -0,0 +1,12 @@
+function toggleMenu() {
+    let menuIcon = document.querySelector(".toggleMenu");
+    console.log(menuIcon.innerHTML);
+    if (menuIcon.innerHTML == "left_panel_close") {
+        menuIcon.innerHTML = "left_panel_open";
+        menuIcon.title = "Click to expand the menu panel"
+    }
+    else if (menuIcon.innerHTML == "left_panel_open") {
+        menuIcon.innerHTML = "left_panel_close";
+        menuIcon.title = "Click to collapse the menu panel"
+    }
+}
