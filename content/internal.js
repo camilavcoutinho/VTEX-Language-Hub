3ed0cf17d5b316a2370fe3a6dd7a68b26950c277
georgelimadev <george.lima@vtex.com.br>
2023-10-11 09:08:39 -0300

    replicating navigation behavior to all menu's sections and items

--- content/internal.js
+++ content/internal.js
@@ -5,11 +5,7 @@ hamburger.addEventListener("click", function () {
 
 function handleInternal() {
 	var currentUrl = window.location.href;
-	if (currentUrl.includes("styleGuides")) {
-		document.getElementById("main-frame").src = "./loc_style_guides/Localization Style Guide - EN/VTEXLocalizationStyleGuideEN.html";
-		expandItem("styleGuides");
-	}
-	else if (currentUrl.includes("localization")) {
+	if (currentUrl.includes("localization")) {
 		document.getElementById("main-frame").src = "./loc_style_guides/Localization Style Guide - EN/VTEXLocalizationStyleGuideEN.html";
 		expandItem("styleGuides");
 	}
@@ -43,10 +39,22 @@ function handleInternal() {
 	}
 	else if (currentUrl.includes("glossaries")) {
 		document.getElementById("main-frame").src = ""; //temporary link
-		// expandItem("glossaries");
+		expandItem("glossaries");
 	}
 	else if (currentUrl.includes("reviewLogs")) {
 		document.getElementById("main-frame").src = "./loc_admin_review_logs/Admin String Standardization - EN/VTEXAdminstringstandardizationinEnglish.html";
+		expandItem("reviewLogs");
+	}
+	else if (currentUrl.includes("enAdminStd")) {
+		document.getElementById("main-frame").src = "./loc_admin_review_logs/Admin String Standardization - EN/VTEXAdminstringstandardizationinEnglish.html";
+		expandItem("reviewLogs");
+	}
+	else if (currentUrl.includes("esAdminStd")) {
+		document.getElementById("main-frame").src = "./loc_admin_review_logs/Admin String Standardization - ES/VTEXAdminstringstandardizationinSpanish.html";
+		expandItem("reviewLogs");
+	}
+	else if (currentUrl.includes("ptAdminStd")) {
+		document.getElementById("main-frame").src = "./loc_admin_review_logs/Admin String Standardization - PT/VTEXAdminstringstandardizationinPortuguese.html";
 		expandItem("reviewLogs");
 	}
 	else if (currentUrl.includes("translationMemories")) {
@@ -116,73 +124,128 @@ function expandStyleGuides() {
 	}
   }
   
-
-function expandReviewLogs() {
+  function expandReviewLogs() {
 	let item = document.getElementById("reviewLogs");
 	let list = item.querySelector("ul");
 	let button = document.querySelector("#collapseReviewLogs");
+  
 	if (list) {
-		item.removeChild(list);
-		button.textContent = "expand_more"
+	  item.removeChild(list);
+	  button.textContent = "expand_more";
+	} else {
+	  button.textContent = "expand_less";
+	  list = document.createElement("ul");
+  
+	  let items = [
+		{ id: "enAdminStd", text: "EN Admin Standardization", link: "./internal.html?id=enAdminStd" },
+		{ id: "esAdminStd", text: "ES Admin Standardization", link: "./internal.html?id=esAdminStd" },
+		{ id: "ptAdminStd", text: "PT Admin Standardization", link: "./internal.html?id=ptAdminStd" },
+	  ];
+  
+	  items.forEach((itemData) => {
+		let listItem = document.createElement("li");
+		let link = document.createElement("a");
+		link.innerText = itemData.text;
+		link.href = itemData.link;
+		listItem.id = itemData.id;
+  
+		// Remove margin and padding for list items
+		listItem.style.margin = "0.5";
+		listItem.style.padding = "0.3";
+  
+		// You can also reduce the margin or padding for the anchor elements if needed
+		link.style.margin = "0";
+		link.style.padding = "0";
+  
+		listItem.appendChild(link);
+		list.appendChild(listItem);
+	  });
+  
+	  list.style.fontWeight = "normal"; // Set font weight for the entire list
+	  item.appendChild(list);
 	}
-	else {
-		button.textContent = "expand_less";
-		list = document.createElement("ul");
-		let listItem1 = document.createElement("li");
-		listItem1.innerText = "EN Admin Standardization";
-		let listItem2 = document.createElement("li");
-		listItem2.innerText = "ES Admin Standardization";
-		let listItem3 = document.createElement("li");
-		listItem3.innerText = "PT Admin Standardization";
-		list.appendChild(listItem1);
-		list.appendChild(listItem2);
-		list.appendChild(listItem3);
-		item.appendChild(list);
-	}
-}
-function expandGlossaries() {
+  }
+  
+  function expandGlossaries() {
 	let item = document.getElementById("glossaries");
 	let list = item.querySelector("ul");
 	let button = document.querySelector("#collapseGlossaries");
+  
 	if (list) {
-		item.removeChild(list);
-		button.textContent = "expand_more";
+	  item.removeChild(list);
+	  button.textContent = "expand_more";
+	} else {
+	  button.textContent = "expand_less";
+	  list = document.createElement("ul");
+  
+	  let items = [
+		{ id: "item1", text: "Item 1", link: "#" },
+		{ id: "item2", text: "Item 2", link: "#" },
+		{ id: "item3", text: "Item 3", link: "#" },
+	  ];
+  
+	  items.forEach((itemData) => {
+		let listItem = document.createElement("li");
+		let link = document.createElement("a");
+		link.innerText = itemData.text;
+		link.href = itemData.link;
+		listItem.id = itemData.id;
+  
+		// Remove margin and padding for list items
+		listItem.style.margin = "0.5";
+		listItem.style.padding = "0.3";
+  
+		// You can also reduce the margin or padding for the anchor elements if needed
+		link.style.margin = "0";
+		link.style.padding = "0";
+  
+		listItem.appendChild(link);
+		list.appendChild(listItem);
+	  });
+  
+	  list.style.fontWeight = "normal"; // Set font weight for the entire list
+	  item.appendChild(list);
 	}
-	else {
-		button.textContent = "expand_less";
-		list = document.createElement("ul");
-		let listItem1 = document.createElement("li");
-		listItem1.innerText = "Item 1";
-		let listItem2 = document.createElement("li");
-		listItem2.innerText = "Item 2";
-		let listItem3 = document.createElement("li");
-		listItem3.innerText = "Item 3";
-		list.appendChild(listItem1);
-		list.appendChild(listItem2);
-		list.appendChild(listItem3);
-		item.appendChild(list);
-	}
-}
-function expandTranslationMemories() {
+  }
+  
+  function expandTranslationMemories() {
 	let item = document.getElementById("translationMemories");
 	let list = item.querySelector("ul");
 	let button = document.querySelector("#collapseTranslationMemories");
+  
 	if (list) {
-		item.removeChild(list);
-		button.textContent = "expand_more";
+	  item.removeChild(list);
+	  button.textContent = "expand_more";
+	} else {
+	  button.textContent = "expand_less";
+	  list = document.createElement("ul");
+  
+	  let items = [
+		{ id: "item1", text: "Item 1", link: "#" },
+		{ id: "item2", text: "Item 2", link: "#" },
+		{ id: "item3", text: "Item 3", link: "#" },
+	  ];
+  
+	  items.forEach((itemData) => {
+		let listItem = document.createElement("li");
+		let link = document.createElement("a");
+		link.innerText = itemData.text;
+		link.href = itemData.link;
+		listItem.id = itemData.id;
+  
+		// Remove margin and padding for list items
+		listItem.style.margin = "0.5";
+		listItem.style.padding = "0.3";
+  
+		// You can also reduce the margin or padding for the anchor elements if needed
+		link.style.margin = "0";
+		link.style.padding = "0";
+  
+		listItem.appendChild(link);
+		list.appendChild(listItem);
+	  });
+  
+	  list.style.fontWeight = "normal"; // Set font weight for the entire list
+	  item.appendChild(list);
 	}
-	else {
-		button.textContent = "expand_less";
-		list = document.createElement("ul");
-		let listItem1 = document.createElement("li");
-		listItem1.innerText = "Item 1";
-		let listItem2 = document.createElement("li");
-		listItem2.innerText = "Item 2";
-		let listItem3 = document.createElement("li");
-		listItem3.innerText = "Item 3";
-		list.appendChild(listItem1);
-		list.appendChild(listItem2);
-		list.appendChild(listItem3);
-		item.appendChild(list);
-	}
-}
+  }  
